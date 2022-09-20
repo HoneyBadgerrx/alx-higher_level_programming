67add45b8462fb83bf6a1a6d280ca808ab9d013d
HoneyBadgerrx <katoyebir@gmail.com>
2022-09-20 09:07:01 -0700

    done

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -3,11 +3,27 @@
  * insert_node - inserts at node
  * @head: pointer to head
  * @number: index to be inserted
+ * Return: pointer to node or NULL if failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+	listint_t *new;
+
 	if (head == NULL)
 		return (NULL);
-	return (add_nodeint_end(head, number));
+	while (head != NULL)
+	{
+		if (head->n <= number)
+		{
+			new = malloc(sizeof (listint_t));
+			if (new == NULL)
+				return (NULL);
+			new->n = number;
+			new->next = head->next;
+			head->next = new;
+			return (new);
+		}
+		head = head->next;
+	}
 	return (NULL);
 }
