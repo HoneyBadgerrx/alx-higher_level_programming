9e74437c28a947966217d2e22769a84aeaae9d60
HoneyBadgerrx <katoyebir@gmail.com>
2022-09-20 12:09:53 -0700

    switched to string format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,4 +2,4 @@
 for i in reversed(range(ord('a'), ord('z') + 1)):
     if i % 2 != ord('z') % 2:
         i = i - (ord('a') - ord('A'))
-    print(f"{i:c}", end='')
+    print("{:c}".format(i), end='')
