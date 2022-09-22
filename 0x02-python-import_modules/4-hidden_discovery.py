337b90d2a246b6634168d89ee155733e741683c3
HoneyBadgerrx <katoyebir@gmail.com>
2022-09-22 12:57:09 -0700

    secret santa

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,4 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     for i, s in enumerate(dir(hidden_4)):
+        if s[0] == '_' and s[1] == '_':
+            continue
         print("{}".format(s))
