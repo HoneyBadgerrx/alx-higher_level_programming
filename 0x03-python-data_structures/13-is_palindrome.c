4bacf1458eb6b010f69cda3932c69220086fe811
HoneyBadgerrx <katoyebir@gmail.com>
2022-09-26 04:46:56 -0700

    revised

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,29 +1,73 @@
 #include "lists.h"
 /**
+ * reverse - reverses the list
+ * @head = pointer to pinter to head
+ * Return: void
+ */
+void reverse(listint_t **head)
+{
+	listint_t *current = *head, *prev = NULL, *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+}
+/**
+ * compare - compares 2 linked lists
+ * @head: first pointer
+ * @slow: 2nd pointer
+ * @isodd: checks for if list is odd
+ * Return : o if not pslin, 1 otherwise
+ */
+int compare(listint_t *head, listint_t *slow, bool isodd)
+{
+	while (head)
+	{
+		if (head->n != slow->n)
+			return (0);
+		head = head->next;
+		slow = slow->next;
+	}
+	if (isodd == true && head == NULL && slow->next == NULL)
+		return (1);
+	if (isodd == false && head == NULL && slow == NULL)
+		return (1);
+	return (0);
+}
+/**
  * is_palindrome - checks fo rpalin
  * @head: pointer to ead
  * Return: 0 if not, 1 if it is
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *p = *head, *q = *head;
-	int count = 0, i, j;
+	listint_t *fast = *head, *slow = *head, *slower;
+	bool isodd = false;
+	int palin;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-
-	for (; q->next; count++)
-		q = q->next;
-	for (i = 0;; ++i)
+	while (slow && fast && fast->next)
 	{
-		q = p;
-		for (j = 0; j < count - i * 2; ++j)
-			q = q->next;
-		if (i > j || j == i)
-			return (1);
-		if (q->n != p->n)
-			break;
-		p = p->next;
+		fast = fast->next->next;
+		slower = slow;
+		slow = slow->next;
 	}
-	return (0);
+	if (fast != NULL)
+	{
+		isodd = true;
+		reverse(&slow);
+	}
+	else
+		reverse(&slow);
+	slower->next = NULL;
+	palin = compare(*head, slow, isodd);
+	reverse(&slow);
+	slower->next = slow;
+	return (palin);
 }
