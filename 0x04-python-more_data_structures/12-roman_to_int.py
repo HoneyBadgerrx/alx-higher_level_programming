ed5933ffaaa001951f07692a62c827a8a0de2b14
HoneyBadgerrx <katoyebir@gmail.com>
2022-09-28 10:20:22 -0700

    typo

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(r):
     if r and type(r) == str:
         s = 0
         rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'M': 1000, 'D': 500, 'C': 100}
-        for i in range(r):
+        for i in range(len(r)):
             if i > 0 and rom[r[i]] > rom[r[i - 1]]:
                 s = s + rom[r[i]] - 2 * rom[r[i - 1]]
             else:
