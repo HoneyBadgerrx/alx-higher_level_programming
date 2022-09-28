3c6ef6880f08f114359ac65a93edffe44b40e6b4
HoneyBadgerrx <katoyebir@gmail.com>
2022-09-28 10:14:13 -0700

    revised

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string or type(roman_string) == str:
+    if roman_string and type(roman_string) == str:
         s = 0
         rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'M': 1000, 'D': 500, 'C': 100}
         for i in range(roman_string):
