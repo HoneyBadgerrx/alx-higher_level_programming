c1258717c6a8fbefe881602b6d94134eda9d7088
HoneyBadgerrx <katoyebir@gmail.com>
2022-09-28 08:01:36 -0700

    removed unit test

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,3 @@ def square_matrix_simple(matrix=[]):
     for i in matrix:
         newmatrix.append(list(map((lambda x: x * x), i)))
     return newmatrix
-print (square_matrix_simple([[1,2,3], [4,5,5]]))
