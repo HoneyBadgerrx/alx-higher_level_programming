e87e5c99ff9c672de99ff5c0461e14ad9116e281
HoneyBadgerrx <katoyebir@gmail.com>
2022-10-11 09:53:14 -0700

    v

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -27,3 +27,25 @@ class Square:
             area of square
         """
         return (self.__size) ** 2
+    @property
+    def size(self):
+        """returns size
+
+        Returns:
+            size
+        """
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """sets value of size
+
+        Args:
+            value (int): value of size
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
