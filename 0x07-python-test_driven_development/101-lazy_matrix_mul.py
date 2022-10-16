40462ef6f25af8aba956c63e15bd18c63fdb5c73
HoneyBadgerrx <katoyebir@gmail.com>
2022-10-16 09:09:09 -0700

    done

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-Defines lazy_matrix function
+lazy mat
 """
 import numpy
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """ calculates the matrix multiplication of two matrices"""
+    """ imatrix multiplication"""
     return numpy.matmul(m_a, m_b)
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-This is the "2-matrix_divided" module.
+module to divide all elements of a matrix
 
-The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
 """
 
 
 def matrix_divided(matrix, div):
-    """Divides all elements in the matrix by div"""
+    """Divide matrix by div"""
     if type(matrix) is not list:
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
