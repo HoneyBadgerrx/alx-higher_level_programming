b8ba1204c44f722b36e48d4285a56653b873a66e
Kabir Atoyebi <katoyebir@gmail.com>
2022-10-24 11:43:47 -0700

    p

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,7 +4,19 @@ rectangle class
 """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry:
+    """exception class"""
+
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates int for corrrect type and if <= 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
@@ -15,3 +27,11 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """returns area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """infrormal repr"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
