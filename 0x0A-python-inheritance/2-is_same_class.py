32699bd0aab3a38c4d3ecf7690ebc90f9793d49c
HoneyBadgerrx <katoyebir@gmail.com>
2022-10-24 09:37:53 -0700

    2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+module to check iobject is instance of class
+"""
+
+
+def is_same_class(obj, a_class):
+    """checks if obj is instance of class"""
+    return isinstance(obj, a_class)
