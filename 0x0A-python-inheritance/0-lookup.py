c5bd7178e31b76fd65049ca26c2e97e4ac81791c
HoneyBadgerrx <katoyebir@gmail.com>
2022-10-24 06:07:42 -0700

    a

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+lookup with dir func
+"""
+
+
+def lookup(obj):
+    """returns attrtibutes and methods of obj"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+a class inheriting list class
+"""
+
+class Mylist(list):
+    """a class inheriting from list"""
+    def print_sorted(self):
+        """prints sorted list in ascending order"""
+        return sorted(self)
