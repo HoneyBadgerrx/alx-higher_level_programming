38eda9a7a1d2b63fff2bbc99aec5d6d3e45ed189
HoneyBadgerrx <katoyebir@gmail.com>
2022-10-24 06:24:52 -0700

    typo

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,11 +4,8 @@ a class inheriting list class
 """
 
 
-class Mylist(list):
+class MyList(list):
     """a class inheriting from list"""
-    def __init__(self):
-        """initialization of object"""
-        super().__init__()
     def print_sorted(self):
         """prints sorted list in ascending order"""
         print(sorted(self))
